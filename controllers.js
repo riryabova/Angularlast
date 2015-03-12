36dcb26c2ca8066b47b1088baa0c8a63d8e72809
Rita Ryabova <riryabova@mail.ru>
2015-03-12 16:33:43 +0500

    lab6
    lab6

---
+++ controllers.js
@@ -0,0 +1,45 @@
+﻿angular
+    .module("app", ["filtersModule", "directivesModule"])
+    .controller("Contorller1", ["$scope",
+        function ($scope) {
+
+            $scope.name = "fill";
+            $scope.filterName = "";
+            $scope.items = [];
+            $scope.items2 = [];
+            $scope.newItem = {
+                name: "",
+                priority: "",
+                author: ""
+            };
+            $scope.add = function () {
+                if (($scope.newItem.name != "") && ($scope.newItem.author != "") && (($scope.newItem.priority == "high") || ($scope.newItem.priority == "medium") || ($scope.newItem.priority == "low"))) {
+                    $scope.items.push({
+                        name: $scope.newItem.name,
+                        priority: $scope.newItem.priority,
+                        author: $scope.newItem.author
+                    });
+                    $scope.newItem.name = "";
+                    $scope.newItem.priority = "";
+                    $scope.newItem.author = "";
+                } else {
+                    alert("Error");
+                }
+            };
+
+
+            $scope.del = function (item) {
+                var index = $scope.items.indexOf(item);
+                $scope.items.splice(index, 1);
+
+            }
+            $scope.replace = function (mname, mpr, misp, i) {
+                $scope.items2.push({
+                    name: mname,
+                    priority: mpr,
+                    author: misp
+                });
+                $scope.del(i);
+            };
+
+        }]);
---
+++ prcolor.js
@@ -0,0 +1,26 @@
+﻿angular
+    .module("directivesModule", [])
+    .directive("prioritycolor", function () {
+
+        return {
+            restrict: "A",
+            require: "?ngModel",
+            link: function ($scope, $element, attrs, ngModel) {
+                if ($scope.i.priority == "high") {
+
+                    $element.css({ "color": "red" });
+
+                }
+                if ($scope.i.priority == "medium") {
+
+                    $element.css({ "color": "yellow" });
+
+                }
+                if ($scope.i.priority == "low") {
+
+                    $element.css({ "color": "green" });
+
+                }
+            }
+        };
+    });
